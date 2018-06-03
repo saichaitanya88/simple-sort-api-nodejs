798a21664059bb188ceb1b63cd007dbe7d5ccd7d
Sai Chaitanya <saichaitanya88@gmail.com>
2018-06-03 15:26:41 -0400

    cleaned up index.js

--- src/index.js
+++ src/index.js
@@ -7,12 +7,7 @@ const app = express();
 // parse application/json
 app.use(bodyParser.json())
 
-
-app.get('/', (req, res) => res.send('Hello World!'));
 app.post('/mergeSort', mergeSortHandler);
 app.post('/bubbleSort', bubbleSortHandler);
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
-
-// necessary for integration testing
-export default app;
